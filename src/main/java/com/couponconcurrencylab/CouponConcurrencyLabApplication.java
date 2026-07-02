c575bbaa86af125ed565086682d88f00c36ce604
rjswjddn <rjswjddn@gmail.com>
2026-07-02 14:08:57 +0900

    First Commit

---
+++ src/main/java/com/couponconcurrencylab/CouponConcurrencyLabApplication.java
@@ -0,0 +1,13 @@
+package com.couponconcurrencylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponConcurrencyLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponConcurrencyLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couponconcurrencylab/CouponConcurrencyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponconcurrencylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponConcurrencyLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
